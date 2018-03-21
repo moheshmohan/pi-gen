53d5d0b8951d85629b46dcae0de5eecefa1fcc74
Mohesh Mohan <moheshmohan@gmail.com>
2018-03-21 09:17:36 +0400

    1.8 inch chinese generic lcd support

---
+++ stage3/02-lcd-tweaks/01-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+
+install -v -d					"${ROOTFS_DIR}/etc/modprobe.d"
+install -v -m 644 files/etc/modprobe.d/fbtft.conf		"${ROOTFS_DIR}/etc/modprobe.d/"
+install -v -m 644 files/etc/modules		"${ROOTFS_DIR}/etc/"
+
+
+install -v -d					"${ROOTFS_DIR}/etc/modules-load.d"
+install -v -m 600 files/etc/modules-load.d/fbtft.conf	"${ROOTFS_DIR}/etc/modules-load.d/"
+
+install -v -d					"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d"
+install -v -m 600 files/usr/share/X11/xorg.conf.d/99-fbdev.conf	"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
